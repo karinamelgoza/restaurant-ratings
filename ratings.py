7995b1d676a8b02cac57b09cf41bc3192b2f9e2e
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-15 15:50:54 -0700

    Add user choices

--- ratings.py
+++ ratings.py
@@ -5,6 +5,8 @@
 
 # create dictionary where ratings will be stored
 from os import sep
+import random
+from typing import List
 
 
 ratings = {}
@@ -17,7 +19,6 @@ f = open('scores.txt', 'r')
 # print(f.readlines()[0])
 
 # loop through each line in .txt file and split it into strings at the :
-
 for i in f:
     splitline = i.split(':')
     # add key value pair to dictionary
@@ -28,11 +29,14 @@ f.close()
 # print(ratings)
 
 
+# print("If you would like to ...\n see restaurant ratings, please type: ratings\n rate a new restaurant, please type: rate\n quit, please type: quit")
+# user_input = input()
+
+
 def alph_sort():
     # stores key value pairs as tuples in a list
     x = ratings.items()
     # print(x)
-
     ratings_sorted = sorted(x)
     # print(ratings_sorted)
 
@@ -41,15 +45,49 @@ def alph_sort():
         print(i[0], ' is rated at ', i[1], '.', sep='')
 
 
-alph_sort()
-
-
 def add_ratings():
-    new_res = input('add restaurant name\n')
-    new_score = input('add restaurant score\n')
-    ratings[new_res] = new_score
-    alph_sort()
+    new_res = input('Add restaurant name:\n')
+    new_score = input('Score the restaurant from 1-5:\n')
+    while int(new_score) not in range(1, 6):
+        new_score = input('Please enter a number between 1-5:')
+    else:
+        ratings[new_res.title()] = new_score
+    # alph_sort()
     # add_ratings()
 
 
-add_ratings()
+# def user_options():
+#     if user_input == 'ratings':
+#         alph_sort()
+#     elif user_input == 'rate':
+#         add_ratings()
+
+
+# user_options()
+
+# def rate_random():
+#     updated_score = input('new score between 1-5:\n')
+#     ran_res = random.choice(list(ratings.items()))
+#     list_con = list(ran_res)
+#     print(list_con)
+#     list_con[1] = updated_score
+
+
+def initial_input():
+    print("If you would like to ...\n see restaurant ratings, please type: ratings\n rate a new restaurant, please type: rate\n quit, enter any letter key")
+    user_input = input()
+
+    def user_options():
+        if user_input == 'ratings':
+            alph_sort()
+            initial_input()
+        elif user_input == 'rate':
+            add_ratings()
+            initial_input()
+        else:
+            print('Have a nice day!')
+
+    user_options()
+
+
+initial_input()
